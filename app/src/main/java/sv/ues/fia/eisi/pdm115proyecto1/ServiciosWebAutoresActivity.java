3656ec0a3a502a0de03f6786d605aab281a5f360
co2093 <coh.2093@gmail.com>
2021-05-31 22:16:01 -0600

    ServicioWebAutor

--- app/src/main/java/sv/ues/fia/eisi/pdm115proyecto1/ServiciosWebAutoresActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/pdm115proyecto1/ServiciosWebAutoresActivity.java
@@ -2,13 +2,77 @@ package sv.ues.fia.eisi.pdm115proyecto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import org.json.JSONObject;
+
+@SuppressLint("NewApi")
 
 public class ServiciosWebAutoresActivity extends AppCompatActivity {
+
+
+    EditText editTextIdAutor;
+    EditText editTextNombreAutor;
+    ImageButton regresar;
+
+    private final String urlHostingGratuito = "https://proyecto1pdm115.000webhostapp.com/ws_autor_insert.php";
+
+    @SuppressLint("NewApi")
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicios_web_autores);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        editTextIdAutor = findViewById(R.id.editTextAutorIDSW);
+        editTextNombreAutor = findViewById(R.id.editTextAutorSW);
+
+        regresar =  findViewById(R.id.btnRegresarAutorSW);
+
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MenuServiciosWeb.class);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
+    }
+
+    public void insertarAutoresWS(View v) {
+
+        if(editTextIdAutor.getText().toString().isEmpty() || editTextNombreAutor.getText().toString().isEmpty()){
+            Toast.makeText(ServiciosWebAutoresActivity.this, "Todos los campos son obligatorios", Toast.LENGTH_LONG).show();
+        }else {
+
+            String id = editTextIdAutor.getText().toString();
+            String nombre = editTextNombreAutor.getText().toString();
+
+
+            String url = null;
+            JSONObject datosNota = new JSONObject();
+            JSONObject nota = new JSONObject();
+            switch (v.getId()) {
+                case R.id.btnAgregarAutorSW:
+                    url = urlHostingGratuito+ "?id=" + id + "&nombre="
+                            + nombre;
+                    Controlador.insertarAutorExterno(url, this);
+                    Toast.makeText(ServiciosWebAutoresActivity.this, "Registro ingresado", Toast.LENGTH_LONG).show();
+                    break;
+            }
+
+        }
+
     }
 }
